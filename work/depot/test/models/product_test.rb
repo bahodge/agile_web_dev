80907aec8a0598c19f232a9d381ebc4626273835
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-06-20 19:29:52 -0500

    woot

---
+++ work/depot/test/models/product_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          image_url: "zzz.jpg",
+                          price: 1)
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+  test "product is not valid without a unique title - il8n" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          image_url: "zzz.jpg",
+                          price: 1)
+    assert product.invalid?
+    assert_equal ["I19n.translate('errors.messages.taken"],
+                 product.errors[:title]
+  end
+
+
+
+  test "product attributes must not be empty " do
+    product = Product.new(title: "My book",
+                          description: "yyy",
+                          image_url: "zzz.jpg",
+                          price: 1)
+    assert product.valid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "My book",
+                          description: "yyy",
+                          image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+                 product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+                 product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+
+  end
+
+end
